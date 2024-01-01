ac763827a04ebf6cb6860c586ddf9f84ab449a53
Abhay Kumar <113308962+httpsabhi@users.noreply.github.com>
2024-01-01 17:37:44 +0530

    Added Responsive Design

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 const apiKey = "111ffe4edf261dd4be5b605664a5c24a";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(city) {
-  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
-  if (response.status == 404) {
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".weather").style.display = "none";
-  } else {
-    var data = await response.json();
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
+function displayData(data){
+  document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
-      Math.round(data.main.temp) + "°c";
-    document.querySelector(".description").innerHTML = data.weather[0].main;
+      Math.round(data.main.temp) + "°C";
+    document.querySelector("#desc").innerHTML = data.weather[0].main;
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector("#feel").innerHTML = data.main.feels_like + "°C";
 
     if (data.weather[0].main == "Clouds") {
       weatherIcon.src = "images/clouds.png";
@@ -37,10 +30,47 @@ async function checkWeather(city) {
     } else if (data.weather[0].main == "Smoke") {
       weatherIcon.src = "images/smoke.png";
     }
+}
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + `&q=${city}&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    console.log(data);
+    displayData(data);
+
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
   }
 }
+
+function geolocation(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else{
+    console.log("Error");
+  }
+}
+
+function showPosition(data){
+  console.log(data)
+  let lat = data.coords.latitude;
+  let lon = data.coords.longitude;
+  fetch(apiUrl + `&lat=${lat}&lon=${lon}&appid=${apiKey}`).then((res)=>res.json()).then((data)=>{
+    console.log(data);
+    displayData(data);
+    document.querySelector("#current-location").innerHTML = data.name;
+    document.querySelector("#current-location-temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".weather").style.display = "block";
+  })
+}
+
+geolocation()
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
